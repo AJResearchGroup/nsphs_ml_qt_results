1d74dd81a3ab2c0be29a230dfbf8499ccee68bf4
richelbilderbeek <richel@richelbilderbeek.nl>
2022-04-11 18:31:20 +0200

    EOD

--- issue_24/run_plink.sh
+++ issue_24/run_plink.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-~/.local/share/plinkr/plink_1_9_unix/plink \
-  --bfile issue_24 \
-  --pheno issue_24.phe \
-  --snp snp_5 --window 2 \
-  --make-bed --out issue_24_subset
+~/.local/share/plinkr/plink_1_9_unix/plink --bfile issue_24 --snp snp_5 --window 0.002 --make-bed --out issue_24_subset ; cat issue_24_subset.bim
+
+
+# plink --file data --extract mysnps.txt 
--- issue_24/view_subset.R
+++ issue_24/view_subset.R
@@ -4,3 +4,4 @@ bed_filename <- "issue_24_subset.bed"
 bim_filename <- "issue_24_subset.bim"
 fam_filename <- "issue_24_subset.fam"
 plink_bin_data <- plinkr::read_plink_bin_data(base_input_filename = base_input_filename)
+plink_bin_data
