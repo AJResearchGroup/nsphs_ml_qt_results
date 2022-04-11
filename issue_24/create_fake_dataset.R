aab968af1bc74edcc0c2ca85098048983d66e18f
richelbilderbeek <richel@richelbilderbeek.nl>
2022-04-11 12:32:57 +0200

    Can create fake dataset

---
+++ issue_24/create_fake_dataset.R
@@ -0,0 +1,8 @@
+setwd("~/GitHubs/nsphs_ml_qt_results/issue_24/")
+filenames <- gcaer::create_gcae_input_files_1(
+  base_input_filename = "issue_24",
+  n_individuals = 10,
+  n_traits = 1,
+  n_snps_per_trait = 10
+)
+
---
+++ issue_24/run_create_fake_dataset.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+Rscript create_fake_dataset.R
