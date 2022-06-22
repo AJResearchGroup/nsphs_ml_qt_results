a07874bf0d7056aa775142fd7e34112698da49e0
richelbilderbeek <richel@richelbilderbeek.nl>
2022-06-22 11:24:04 +0200

    Create GC in time for https://github.com/AJResearchGroup/richel/issues/186

--- issue_42_20220622/analyse_issue_42_results.R
+++ issue_42_20220622/analyse_issue_42_results.R
@@ -14,6 +14,86 @@ testthat::expect_equal(2, length(pheno_model_ids_that_work))
 window_kbs_that_work <- unique(sort(matches[, 4]))
 testthat::expect_equal(4, length(window_kbs_that_work))
 
+#############################################################################
+t_combinations <- tidyr::expand_grid(
+  model_id = model_ids_that_work,
+  pheno_model_id = pheno_model_ids_that_work,
+  window_kb = window_kbs_that_work
+)
+t_combinations$folder_name <- paste0(
+    "data_issue_42_", t_combinations$model_id,
+    "_", t_combinations$pheno_model_id, "_", t_combinations$window_kb,
+    "_ae"
+)
+t_combinations$log_filename_25 <- paste0(
+  "25_run_issue_42_", t_combinations$model_id,
+  "_", t_combinations$pheno_model_id, "_", t_combinations$window_kb,
+  ".log"
+)
+t_combinations$folder_exists <- dir.exists(t_combinations$folder_name)
+t_combinations$log_filename_25_exists <- file.exists(t_combinations$log_filename_25)
+#############################################################################
+# Explore when dir does not exist, but log file does
+#############################################################################
+if ("caused by" == "concurrency") {
+  is_interesting <- t_combinations$log_filename_25_exists & !t_combinations$folder_exists
+  interesting_log_files <- t_combinations$log_filename_25[is_interesting]
+  readLines(interesting_log_files)
+  interesting_log_files
+  file.copy(from = interesting_log_files, to = "~/25_run_issue_42_M1_p1_100.log")
+}
+#############################################################################
+# Genotype concordance in time
+#############################################################################
+genotype_concordance_in_time_list <- list()
+for (i in seq_len(nrow(t_combinations))) {
+  this_row <- t_combinations[i, ]
+  filename <- file.path(
+    this_row$folder_name,
+    "genotype_concordances.csv"
+  )
+  if (!file.exists(filename)) {
+    message(filename, " does not exist")
+    next
+  }
+  t_genotype_concordances_in_time <- gcaer::read_genotype_concordances_file(
+    filename
+  )
+  t_genotype_concordances_in_time$model_id <- this_row$model_id
+  t_genotype_concordances_in_time$pheno_model_id <- this_row$pheno_model_id
+  t_genotype_concordances_in_time$window_kb <- this_row$window_kb
+  genotype_concordance_in_time_list[[i]] <- t_genotype_concordances_in_time
+}
+t_genotype_concordances_in_time <- dplyr::bind_rows(genotype_concordance_in_time_list)
+
+window_kb_to_data <- function(window_kb) {
+  if (window_kb == 1) return("1 kb, 2 SNPs")
+  if (window_kb == 10) return("10 kb, 5 SNPs")
+  if (window_kb == 100) return("100 kb, 5 SNPs")
+  if (window_kb == 1000) return("1000 kb, 6 SNPs")
+  stop("Unknown 'window_kb': ", window_kb)
+}
+
+t_genotype_concordances_in_time$genetic_data <- Vectorize(window_kb_to_data)(t_genotype_concordances_in_time$window_kb)
+
+ggplot2::ggplot(
+  t_genotype_concordances_in_time,
+  ggplot2::aes(x = epoch, y = genotype_concordance, color = genetic_data)
+) + ggplot2::geom_line(size = 1) +
+  ggplot2::scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, by = 0.5)) +
+  ggplot2::scale_x_continuous(breaks = seq(0, 1000, by = 200)) +
+  ggplot2::facet_grid(model_id ~ pheno_model_id) +
+  gcaer::get_gcaer_theme() +
+  ggplot2::theme(
+    axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1),
+    panel.border = ggplot2::element_rect(colour = "black", fill = "transparent")
+  )
+ggplot2::ggsave(filename = "issue_42_genotype_concordances_in_time.png", width = 7, height = 7)
+
+
+###
+### OLD
+###
 t <- tidyr::expand_grid(
   model_id = model_ids_that_work,
   pheno_model_id = pheno_model_ids_that_work,
